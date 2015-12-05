4658811c505cb44c6c7bc672e8e2582432048066
CreamBing <2242731684@qq.com>
2015-12-05 10:12:25 +0800

    新建私有属性hello

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -1,9 +1,17 @@
 package hello;
 
 public class Hello {
-
+	private String hello;
+	public String getHello() {
+		return hello;
+	}
+	public void setHello(String hello) {
+		this.hello = hello;
+	}
 	public Hello() {
 		super();
+		// TODO Auto-generated constructor stub
+		//test
 	}
 	
 }
